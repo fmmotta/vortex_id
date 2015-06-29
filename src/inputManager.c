16bda3bf50ce39dbdbc858e3930d3d146677d1fb
José Hugo Elsas <jhelsas@gmail.com>
2015-06-29 16:16:00 -0300

    starting to write ini file handler

--- src/inputManager.c
+++ src/inputManager.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "ini.h"
-#include ""
+#include "inih/ini.h"
+#include "inputManager.h"
 
+typedef struct {
+  int Width,Height,nVortex,nRuns;
+  int runType,seed,dim,adaptive;
+  int numG,numRc;
+  float Gmin,Gmax,RcMin,RcMax;
+  float *Glist,*Rclist;
+  float swThresh, sndSwThresh;
+  const char *genFile,*tag;
+} configVar;
 
+static int vortexIdHandler(void* user, const char* section, 
+                           const char* name,const char* value)
+{
+  configVar *vConfig = (configVar*)user;
+  
+    #define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+    if (MATCH("Grid_Parameters", "Width")) {
+        vconfig->Width = atoi(value);
+    } else if (MATCH("Grid_Parameters", "Heigth")) {
+        vconfig->Heigth = atoi(value);
+    } else if (MATCH("user", "email")) {
+        vconfig->email = strdup(value);
+    } else {
+        return 0;  /* unknown section/name, error */
+    }
+    
+    return 1; /* returns sucess */
+}
