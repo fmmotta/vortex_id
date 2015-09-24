56bb258820f2476e027e534c648cf29e5fcadd58
jhelsas <jhelsas@gmail.com>
2015-09-23 21:49:53 -0300

    just adding a header entry

--- src/stencilManip.h
+++ src/stencilManip.h
@@ -9,5 +9,8 @@ int UToGradUuniformTorus(int Height,int Width,int type,float *x0,float *dx,
 int UToGradUnonUnifFrame(int Height,int Width,int type,float *x0,float *dx,
                          float *X,float *Y, float *uField, float *gField);
 
+int UToGradUnUnifHalfFrame(int Height,int Width,int type,float *x0,float *dx,
+                           float *X,float *Y, float *uField, float *gField);
+
 int UToGradUnUnifFullFrame(int Height,int Width,int type,float *x0,float *dx,
                            float *X,float *Y, float *uField, float *gField);
