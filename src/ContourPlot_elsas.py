b879aa44c7147dc2791db8e4c18ae83e06b6bdf9
jhelsas <jhelsas@gmail.com>
2015-10-19 00:27:51 -0200

    adding plotscript

---
+++ src/ContourPlot_elsas.py
@@ -0,0 +1,46 @@
+import numpy as np
+import math
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+from scipy.spatial import Voronoi, voronoi_plot_2d
+
+RawData =np.loadtxt("initFOAMsw.txt")
+X,Y,Z	=np.transpose(RawData)
+
+xmax = np.max(X); xmin = np.min(X); Dx = xmax-xmin; xmed=(xmax+xmin)/2;
+ymax = np.max(Y); ymin = np.min(Y); Dy = ymax-ymin; ymed=(ymax+ymin)/2;
+zmax = np.max(Z); zmin = np.min(Z); Dz = zmax-zmin; zmed=(zmax+zmin)/2;
+per = 0.20
+
+print str(xmin)+" "+str(xmax)
+print str(ymin)+" "+str(ymax)
+print str(zmin)+" "+str(zmax)
+
+Data 	= [ [x,y,z] for [x,y,z] in RawData if x<xmax and x> xmin and y<ymax and y>ymin]
+X,Y,Z	= np.transpose(Data)
+#Z	= np.log(Z)
+Nsample= 150
+
+xi = np.linspace(X.min(),X.max(),Nsample)
+yi = np.linspace(Y.min(),Y.max(),Nsample)
+zi = griddata((X, Y), Z, (xi[None,:], yi[:,None]), method='cubic')
+
+fig = plt.figure()
+
+
+plt.contourf(xi, yi, zi ,Nsample)                             
+#plt.clim(0,zmax)
+cb=plt.colorbar(aspect='equal',ticks=[0.,11.56,23.11,34.67,46.228])
+plt.clim(zmin,zmax)    
+#plt.surface().set_clim([zmin,zmax])    
+#cb.set_clim(0, zmax)
+
+#for [x,y,z] in Data:
+#	s=plt.scatter(x,y,color='black',s=0.01)
+
+#plt.tight_layout()
+plt.axes().set_aspect('equal')
+plt.savefig('initFOAMsw.pdf',dpi=600)#,bbox_inches='tight')
+
+#plt.show()
