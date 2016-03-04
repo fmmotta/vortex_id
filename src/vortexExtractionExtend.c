9986ab1d30d26cbc9244e2b9185bb8d48bc74813
José Hugo Elsas <jhelsas@gmail.com>
2016-03-04 16:46:20 -0300

    adding second moment integration to vortex extraction extended

--- src/vortexExtractionExtend.c
+++ src/vortexExtractionExtend.c
@@ -268,6 +268,237 @@ int vortexExtFromVortCurv(int Height,int Width, int nCnect,double *X,double *Y,
   return 0;
 }
 
+inline int add_dx2dy2(int Height,int Width, int i,int j,int ik,int jk,
+                      double *gField,double *XX,double *XY,double *YX,
+                      double *YY,double X[],double Y[])
+{
+  if((i+ik<0) || (i+ik)>=Height){
+    if( (j+jk<0) || (j+jk)>=Width ){
+      *XX = 9.*gField[4*(     i*Width  + j  )+2]*X[j   ]*X[j   ];
+      *XX-= 9.*gField[4*(     i*Width  + j  )+1]*X[j   ]*X[j   ];
+
+      *XY = 9.*gField[4*(     i*Width  + j  )+2]*X[j   ]*Y[i   ];
+      *XY-= 9.*gField[4*(     i*Width  + j  )+1]*X[j   ]*Y[i   ];
+
+      *YX = 9.*gField[4*(     i*Width  + j  )+2]*Y[i   ]*X[j   ];
+      *YX-= 9.*gField[4*(     i*Width  + j  )+1]*Y[i   ]*X[j   ];
+        
+      *YY = 9.*gField[4*(     i*Width  + j  )+2]*Y[i   ]*Y[i   ];
+      *YY-= 9.*gField[4*(     i*Width  + j  )+1]*Y[i   ]*Y[i   ];
+    }
+    else{
+      *XX = 9.*gField[4*(     i*Width  + j  )+2]*X[j   ]*X[j   ]+
+            3.*gField[4*(     i*Width+(j+jk))+2]*X[j+jk]*X[j+jk];
+      *XX-= 9.*gField[4*(     i*Width  + j  )+1]*X[j   ]*X[j   ]+
+            3.*gField[4*(     i*Width+(j+jk))+1]*X[j+jk]*X[j+jk];
+
+      *XY = 9.*gField[4*(     i*Width  + j  )+2]*X[j   ]*Y[i   ]+
+            3.*gField[4*(     i*Width+(j+jk))+2]*X[j+jk]*Y[i   ];
+      *XY-= 9.*gField[4*(     i*Width  + j  )+1]*X[j   ]*Y[i   ]+
+            3.*gField[4*(     i*Width+(j+jk))+1]*X[j+jk]*Y[i   ];
+
+      *YX = 9.*gField[4*(     i*Width  + j  )+2]*Y[i   ]*X[j   ]+
+            3.*gField[4*(     i*Width+(j+jk))+2]*Y[i   ]*X[j+jk];
+      *YX-= 9.*gField[4*(     i*Width  + j  )+1]*Y[i   ]*X[j   ]+
+            3.*gField[4*(     i*Width+(j+jk))+1]*Y[i   ]*X[j+jk];
+
+      *YY = 9.*gField[4*(     i*Width  + j  )+2]*Y[i   ]*Y[i   ]+
+            3.*gField[4*(     i*Width+(j+jk))+2]*Y[i   ]*Y[i   ];
+      *YY-= 9.*gField[4*(     i*Width  + j  )+1]*Y[i   ]*Y[i   ]+
+            3.*gField[4*(     i*Width+(j+jk))+1]*Y[i   ]*Y[i   ];
+    }
+  }
+  else{
+    if( (j+jk<0) || (j+jk)>=Width ){
+      *XX = 9.*gField[4*(     i*Width  + j  )+2]*X[j   ]*X[j   ]+
+            3.*gField[4*((i+ik)*Width  + j  )+2]*X[j   ]*X[j   ];
+      *XX-= 9.*gField[4*(     i*Width  + j  )+1]*X[j   ]*X[j   ]+
+            3.*gField[4*((i+ik)*Width  + j  )+1]*X[j   ]*X[j   ];
+
+      *XY = 9.*gField[4*(     i*Width  + j  )+2]*X[j   ]*Y[i   ]+
+            3.*gField[4*((i+ik)*Width  + j  )+2]*X[j   ]*Y[i+ik];
+      *XY-= 9.*gField[4*(     i*Width  + j  )+1]*X[j   ]*Y[i   ]+
+            3.*gField[4*((i+ik)*Width  + j  )+1]*X[j   ]*Y[i+ik];
+
+      *YX = 9.*gField[4*(     i*Width  + j  )+2]*Y[i   ]*X[j   ]+
+            3.*gField[4*((i+ik)*Width  + j  )+2]*Y[i+ik]*X[j   ];
+      *YX-= 9.*gField[4*(     i*Width  + j  )+1]*Y[i   ]*X[j   ]+
+            3.*gField[4*((i+ik)*Width  + j  )+1]*Y[i+ik]*X[j   ];
+
+      *YY = 9.*gField[4*(     i*Width  + j  )+2]*Y[i+ik]*Y[i+ik]+
+            3.*gField[4*((i+ik)*Width  + j  )+2]*Y[i+ik]*Y[i+ik];
+      *YY-= 9.*gField[4*(     i*Width  + j  )+1]*Y[i+ik]*Y[i+ik]+
+            3.*gField[4*((i+ik)*Width  + j  )+1]*Y[i+ik]*Y[i+ik];
+    }
+    else{
+      *XX = 9.*gField[4*(     i*Width  + j  )+2]*X[j   ]*X[j   ]+
+            3.*gField[4*(     i*Width+(j+jk))+2]*X[j+jk]*X[j+jk]+
+            3.*gField[4*((i+ik)*Width  + j  )+2]*X[j   ]*X[j   ]+
+            1.*gField[4*((i+ik)*Width+(j+jk))+2]*X[j+jk]*X[j+jk];
+      *XX-= 9.*gField[4*(     i*Width  + j  )+1]*X[j   ]*X[j   ]+
+            3.*gField[4*(     i*Width+(j+jk))+1]*X[j+jk]*X[j+jk]+
+            3.*gField[4*((i+ik)*Width  + j  )+1]*X[j   ]*X[j   ]+
+            1.*gField[4*((i+ik)*Width+(j+jk))+1]*X[j+jk]*X[j+jk];
+
+      *XY = 9.*gField[4*(     i*Width  + j  )+2]*X[j   ]*Y[i   ]+
+            3.*gField[4*(     i*Width+(j+jk))+2]*X[j+jk]*Y[i   ]+
+            3.*gField[4*((i+ik)*Width  + j  )+2]*X[j   ]*Y[i+ik]+
+            1.*gField[4*((i+ik)*Width+(j+jk))+2]*X[j+jk]*Y[i+ik];
+      *XY-= 9.*gField[4*(     i*Width  + j  )+1]*X[j   ]*Y[i   ]+
+            3.*gField[4*(     i*Width+(j+jk))+1]*X[j+jk]*Y[i   ]+
+            3.*gField[4*((i+ik)*Width  + j  )+1]*X[j   ]*Y[i+ik]+
+            1.*gField[4*((i+ik)*Width+(j+jk))+1]*X[j+jk]*Y[i+ik];
+
+      *YX = 9.*gField[4*(     i*Width  + j  )+2]*Y[i   ]*X[i   ]+
+            3.*gField[4*(     i*Width+(j+jk))+2]*Y[i   ]*X[i+jk]+
+            3.*gField[4*((i+ik)*Width  + j  )+2]*Y[i+ik]*X[i   ]+
+            1.*gField[4*((i+ik)*Width+(j+jk))+2]*Y[i+ik]*X[i+jk];
+      *YX-= 9.*gField[4*(     i*Width  + j  )+1]*Y[i   ]*X[j   ]+
+            3.*gField[4*(     i*Width+(j+jk))+1]*Y[i   ]*X[j+jk]+
+            3.*gField[4*((i+ik)*Width  + j  )+1]*Y[i+ik]*X[j   ]+
+            1.*gField[4*((i+ik)*Width+(j+jk))+1]*Y[i+ik]*X[j+jk];
+
+      *YX = 9.*gField[4*(     i*Width  + j  )+2]*Y[i   ]*X[i   ]+
+            3.*gField[4*(     i*Width+(j+jk))+2]*Y[i   ]*X[i+jk]+
+            3.*gField[4*((i+ik)*Width  + j  )+2]*Y[i+ik]*X[i   ]+
+            1.*gField[4*((i+ik)*Width+(j+jk))+2]*Y[i+ik]*X[i+jk];
+      *YX-= 9.*gField[4*(     i*Width  + j  )+1]*Y[i   ]*X[j   ]+
+            3.*gField[4*(     i*Width+(j+jk))+1]*Y[i   ]*X[j+jk]+
+            3.*gField[4*((i+ik)*Width  + j  )+1]*Y[i+ik]*X[j   ]+
+            1.*gField[4*((i+ik)*Width+(j+jk))+1]*Y[i+ik]*X[j+jk];
+
+      *YY = 9.*gField[4*(     i*Width  + j  )+2]*Y[i   ]*X[i   ]+
+            3.*gField[4*(     i*Width+(j+jk))+2]*Y[i   ]*Y[i   ]+
+            3.*gField[4*((i+ik)*Width  + j  )+2]*Y[i+ik]*Y[i+ik]+
+            1.*gField[4*((i+ik)*Width+(j+jk))+2]*Y[i+ik]*Y[i+ik];
+      *YY-= 9.*gField[4*(     i*Width  + j  )+1]*Y[i   ]*Y[i   ]+
+            3.*gField[4*(     i*Width+(j+jk))+1]*Y[i   ]*Y[i   ]+
+            3.*gField[4*((i+ik)*Width  + j  )+1]*Y[i+ik]*Y[i+ik]+
+            1.*gField[4*((i+ik)*Width+(j+jk))+1]*Y[i+ik]*Y[i+ik];
+    }
+  }
+
+  *XX *= fabs( (Y[i+ik]-Y[i])*(X[j+jk]-X[j]) )/64.0;
+  *XY *= fabs( (Y[i+ik]-Y[i])*(X[j+jk]-X[j]) )/64.0;
+  *YX *= fabs( (Y[i+ik]-Y[i])*(X[j+jk]-X[j]) )/64.0;
+  *YY *= fabs( (Y[i+ik]-Y[i])*(X[j+jk]-X[j]) )/64.0;
+
+  return 0;
+}
+
+int secondMomentFromVortCurv(int Height,int Width, int nCnect,double *X,double *Y,
+                             double *sField,double *gField,int *label,
+                             double *vCatalog, double *gyreRadiusOut)
+{
+  int i,j,k,err;
+  double G,a,b,rc,dx,dy,dA,dgradU[2][2]; // vorticity
+  double w[nCnect],A[nCnect],a0[nCnect],b0[nCnect];
+  
+  if((Height<=0)||(Width<=0))
+    return -1;
+  if(vCatalog==NULL)
+    return -2;
+  
+  for(k=0;k<nCnect;k+=1){
+    w[k]=0.;A[k]=0.;a0[k]=0.;b0[k]=0.;
+  }
+
+  for(i=0;i<Height;i+=1)
+    for(j=0;j<Width;j+=1){
+      k=label[i*Width+j];
+
+      if((k>=0)&&(k<nCnect)){
+        // ++ quadrant        
+        err=add_dgradU(Height,Width,i,j,1,1,gField,dgradU,X,Y);
+        if(err!=0) return err;
+        w[k] += dgradU[1][0]-dgradU[0][1];
+        
+        err=add_dxdy(Height,Width,i,j,1,1,gField,&dx,&dy,X,Y);
+        if(err!=0) return err;
+        a0[k] += dx;
+        b0[k] += dy;
+
+        err=add_dA(Height,Width,i,j,k,1,1,gField,label,&dA,X,Y);
+        if(err!=0) return err;
+        A[k] += dA;
+        
+        /*************************************************/
+        
+        // -+ quadrant
+        err=add_dgradU(Height,Width,i,j,-1,1,gField,dgradU,X,Y);
+        if(err!=0) return err;
+        w[k] += dgradU[1][0]-dgradU[0][1];
+        
+        err=add_dxdy(Height,Width,i,j,-1,1,gField,&dx,&dy,X,Y);
+        if(err!=0) return err;
+        a0[k] += dx;
+        b0[k] += dy;
+
+        err=add_dA(Height,Width,i,j,k,-1,1,gField,label,&dA,X,Y);
+        if(err!=0) return err;
+        A[k] += dA;
+        
+        /*************************************************/
+
+        // +- quadrant
+        err=add_dgradU(Height,Width,i,j,1,-1,gField,dgradU,X,Y);
+        if(err!=0) return err;
+        w[k] += dgradU[1][0]-dgradU[0][1];
+        
+        err=add_dxdy(Height,Width,i,j,1,-1,gField,&dx,&dy,X,Y);
+        if(err!=0) return err;
+        a0[k] += dx;
+        b0[k] += dy;
+
+        err=add_dA(Height,Width,i,j,k,1,-1,gField,label,&dA,X,Y);
+        if(err!=0) return err;
+        A[k] += dA;
+        
+        /*************************************************/
+        
+        // -- quadrant
+        err=add_dgradU(Height,Width,i,j,-1,-1,gField,dgradU,X,Y);
+        if(err!=0) return err;
+        w[k] += dgradU[1][0]-dgradU[0][1];
+        
+        err=add_dxdy(Height,Width,i,j,-1,-1,gField,&dx,&dy,X,Y);
+        if(err!=0) return err;
+        a0[k] += dx;
+        b0[k] += dy;
+
+        err=add_dA(Height,Width,i,j,k,1,-1,gField,label,&dA,X,Y);
+        if(err!=0) return err;
+        A[k] += dA;
+        
+        /*************************************************/
+      }
+    }
+
+  for(k=0;k<nCnect;k+=1){
+    // 0.977816 corrects for gridsize
+    rc= sqrt(A[k]/M_PI)*(sqrtf(2.));//0.977816); // Constant comming from 
+                                                 //  lamb-oseen vortex;
+    if(fabs(w[k])>0.){
+      a=a0[k]/w[k]; 
+      b=b0[k]/w[k];
+    }
+    else{
+      a=X[0];
+      b=Y[0];
+    }
+    
+    G = 2.541494083*w[k]; // 2.541494083 = 1/(1-1/sqrt(e)) 
+                          // ... should correct for finite grid size?
+    
+    vCatalog[4*k+0] = G;
+    vCatalog[4*k+1] = rc;
+    vCatalog[4*k+2] = a;
+    vCatalog[4*k+3] = b;
+  }
+
+  return 0;
+}
+
 int vortexExtFromSwirlStr(int Height,int Width, int nCnect,double *X,double *Y,
                           double *sField,double *gField,int *label,
                           double **vCatalogOut)
