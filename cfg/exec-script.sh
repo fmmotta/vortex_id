f82822dcdcf85b51c5f35e880e92bf08a4cd253b
jhelsas <jhelsas@gmail.com>
2016-03-25 16:11:52 -0300

    adding bash script for batch executing

---
+++ cfg/exec-script.sh
@@ -0,0 +1,12 @@
+time ./bin/sampledVortexEssayFull cfg/vc-nosub-negNoShear.ini > log/nosub-negNoShear.log &
+time ./bin/sampledVortexEssayFull cfg/vc-nosub-negWeakShear.ini > log/nosub-negWeakShear.log &
+time ./bin/sampledVortexEssayFull cfg/vc-nosub-negStrongShear.ini > log/nosub-negStrongShear.log &
+time ./bin/sampledVortexEssayFull cfg/vc-nosub-posnegNoShear.ini > log/nosub-posnegNoShear.log &
+time ./bin/sampledVortexEssayFull cfg/vc-nosub-posnegWeakShear.ini > log/nosub-posnegWeakShear.log &
+time ./bin/sampledVortexEssayFull cfg/vc-nosub-posnegStrongShear.ini > log/nosub-posnegStrongShear.log &
+time ./bin/sampledVortexEssayFull cfg/vc-uSub-negNoShear.ini > log/uSub-negNoShear.log &
+time ./bin/sampledVortexEssayFull cfg/vc-uSub-negWeakShear.ini > log/uSub-negWeakShear.log &
+time ./bin/sampledVortexEssayFull cfg/vc-uSub-negStrongShear.ini > log/uSub-negStrongShear.log &
+time ./bin/sampledVortexEssayFull cfg/vc-uSub-posnegNoShear.ini > log/uSub-posnegNoShear.log &
+time ./bin/sampledVortexEssayFull cfg/vc-uSub-posnegWeakShear.ini > log/uSub-posnegWeakShear.log &
+time ./bin/sampledVortexEssayFull cfg/vc-uSub-posnegStrongShear.ini > log/uSub-posnegStrongShear.log
