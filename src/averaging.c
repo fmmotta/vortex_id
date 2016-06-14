d500de95ec8dfbf5e445b441a9f2a8ad4f09f609
José Hugo Elsas <jhelsas@gmail.com>
2016-06-14 16:58:24 -0300

    altering again averaging file

--- src/averaging.c
+++ src/averaging.c
@@ -12,8 +12,6 @@ int main(int argc,char **argv){
   double G,rc,x,y;
   double avgGp[Nbins+1],avgRcp[Nbins+1],avgGn[Nbins+1],avgRcn[Nbins+1];
   double avgWp[Nbins+1],avgWn[Nbins+1];
-  double avgG2p[Nbins+1],avgRc2p[Nbins+1],avgG2n[Nbins+1],avgRc2n[Nbins+1];
-  double avgW2p[Nbins+1],avgW2n[Nbins+1];
   FILE *dados;
   
   dy = (y1-y0)/Nbins;
@@ -25,11 +23,9 @@ int main(int argc,char **argv){
   for(k=0;k<=Nbins;k+=1){
     numGp[k] = 0 ; numRcp[k] = 0 ;
     avgGp[k] = 0.; avgRcp[k] = 0.;
+    numGn[k] = 0 ; numRcn[k] = 0 ;
     avgGn[k] = 0.; avgRcn[k] = 0.;
-    avgWn[k] = 0.; avgWp[k] = 0.;
-    avgG2p[k] = 0.; avgRc2p[k] = 0.;
-    avgG2n[k] = 0.; avgRc2n[k] = 0.;
-    avgW2n[k] = 0.; avgW2p[k] = 0.;
+    avgWp[k] = 0.; avgWn[k] = 0.;
   }
 
   while( true ){
@@ -42,67 +38,35 @@ int main(int argc,char **argv){
       numGp[k] += 1; numRcp[k] += 1;
       avgGp[k] += G; avgRcp[k] += rc;
       avgWp[k] += G/(rc*rc);
-
-      avgG2p[k] += G*G; avgRc2p[k] += rc*rc;
-      avgW2p[k] += (G/(rc*rc))*(G/(rc*rc));
     }
     else{
       numGn[k] += 1; numRcn[k] += 1;
       avgGn[k] += G; avgRcn[k] += rc;
       avgWn[k] += G/(rc*rc);
-
-      avgG2n[k] += G*G; avgRc2n[k] += rc*rc;
-      avgW2n[k] += (G/(rc*rc))*(G/(rc*rc));
     }
   }
 
   fclose(dados);
 
-  dados = fopen(argv[2],"w");
-  if(dados==NULL)
-  	printf("Shit!\n");
+  fopen(argv[2],"w");
   for(k=0;k<Nbins;k+=1){
     if(numGp[k]!=0)
       avgGp[k] /= numGp[k];
     if(numGp[k]!=0)
       avgWp[k] /= numGp[k];
-    if(numRcp[k]!=0)
-      avgRcp[k] /= numRcp[k];
+    if(numGp[k]!=0)
+      avgRcp[k] /= numGp[k];
 
     if(numGp[k]!=0)
-      avgG2p[k] /= numGp[k];
-    if(numGp[k]!=0)
-      avgW2p[k] /= numGp[k];
-    if(numRcp[k]!=0)
-      avgRc2p[k] /= numRcp[k];
-
-    if(numGn[k]!=0)
       avgGn[k] /= numGn[k];
-    if(numGn[k]!=0)
+    if(numGp[k]!=0)
       avgWn[k] /= numGn[k];
-    if(numRcn[k]!=0)
-      avgRcn[k] /= numRcn[k];
+    if(numGp[k]!=0)
+      avgRcn[k] /= numGp[k];
 
-    if(numGn[k]!=0)
-      avgG2n[k] /= numGn[k];
-    if(numGn[k]!=0)
-      avgW2n[k] /= numGn[k];
-    if(numRcn[k]!=0)
-      avgRc2n[k] /= numRcn[k];
-
-    fprintf(dados,"%f %f %f %f %f %f %f %f %f %f %f %f %f\n",y0+k*dy
-                                          ,avgGp[k]
-                                          ,sqrt(avgG2p[k]-avgGp[k]*avgGp[k])
-                                          ,avgRcp[k]
-                                          ,sqrt(avgRc2p[k]-avgRcp[k]*avgRcp[k])
-                                          ,avgWp[k]
-                                          ,sqrt(avgW2p[k]-avgWp[k]*avgWp[k])
-                                          ,avgGn[k]
-                                          ,sqrt(avgG2n[k]-avgGn[k]*avgGn[k])
-                                          ,avgRcn[k]
-                                          ,sqrt(avgRc2n[k]-avgRcn[k]*avgRcn[k])
-                                          ,avgWn[k]
-                                          ,sqrt(avgW2n[k]-avgWn[k]*avgWn[k]));
+    fprintf(dados,"%f %f %f %f %f %f %f\n",y0+k*dy,avgGp[k],avgRcp[k]
+                                             ,avgGn[k],avgRcn[k]
+                                             ,avgWp[k],avgWn[k]);
   }
   fclose(dados);
 
